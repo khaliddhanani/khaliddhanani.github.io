216077c8778558fed158c2b5f0ab4febd23c87bb
khaliddhanani <khaliddhanani@gmail.com>
2020-02-22 14:49:14 -0800

    Removed unnecessary assets directory

--- boardgames/Assets/Scripts/SwitchToEditorHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SwitchToEditorHelper : MonoBehaviour {
-    public GameObject obj;
-    public bool prefab;
-}
